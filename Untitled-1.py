d330e7effa7f4694ef424b8d1c5f1faa8d00152e
anujrimal <164611179+anujrimal@users.noreply.github.com>
2024-03-25 03:22:48 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,33 @@
+# %%
+minutes = 60
+hours = 60
+total = minutes * hours
+print (total)
+
+
+# %%
+seconds_per_hour = total
+print (seconds_per_hour)
+
+# %% [markdown]
+# This code finds seconds in an hour by multiplying number of seconds in a minute and number of minute in an hour.
+
+# %%
+seconds_per_hour = 3600
+seconds_per_day = seconds_per_hour * 24
+print (seconds_per_day)
+
+# %%
+seconds_per_day = 86400
+seconds_per_hour = 3600
+result = seconds_per_day / seconds_per_hour 
+print (result)
+
+# %%
+result_integer_division = seconds_per_day // seconds_per_hour
+print(result_integer_division)
+
+# %%
+
+
+
